aedab2969d6144dc4a4001831a66e22cc836e562
webappfirewall <60896858+webappfirewall@users.noreply.github.com>
2020-03-12 17:50:09 -0600

    Add files via upload

--- conexiondb.py
+++ conexiondb.py
@@ -3,4 +3,4 @@ from pymongo import MongoClient
 
 username = urllib.parse.quote_plus('@dm1n')
 passwor = urllib.parse.quote_plus('Qw3rt&.12345')
-client = MongoClient('mongodb://%s:%s@10.0.2.4' % (username, passwor))
+client = MongoClient('mongodb://%s:%s@192.168.17.146' % (username, passwor))
--- modificaciones.py
+++ modificaciones.py
@@ -6,12 +6,10 @@ import urllib.parse
 import conf_ini
 import getpass
 import datetime
-from pymongo import MongoClient
+import conexiondb
 
-# variables globales
-username = urllib.parse.quote_plus('@dm1n')
-passwor = urllib.parse.quote_plus('Qw3rt&.12345')
-client = MongoClient('mongodb://%s:%s@10.0.2.4' % (username, passwor))
+# Variables globales
+client = conexiondb.client
 db = client['waf']
 collection = db['config']
 collection_log = db['log']
